a1be341aeebea895b449f7de17a3372eea255055
Artur Zacniewski <a.zacniewski@gmail.com>
2022-02-06 21:24:19 +0100

    Forecast weather for given place

--- apps/weather/views.py
+++ apps/weather/views.py
@@ -23,10 +23,14 @@ def weather_in_default_location(request):
 
 def current_weather(request):
     query = request.GET.get('weather_query')
-    current_weather_api_url = "https://api.weatherapi.com/v1/current.json"
-    current_weather_api_url += "?key=" + wak + "&q=" + query + "&aqi=no"
-    response = requests.get(current_weather_api_url)
-    print(response.json())
+    current_weather_api_url = f"https://api.weatherapi.com/v1/current.json"
+    current_weather_api_url += f"?key={wak}&q={query}&aqi=no"
+    forecast_weather_api_url = f"https://api.weatherapi.com/v1/forecast.json"
+    forecast_weather_api_url += f"?key={wak}&q={query}&aqi=no"
+    response_current = requests.get(current_weather_api_url)
+    response_forecast = requests.get(forecast_weather_api_url)
+    print(response_current.json())
     return render(request, 'weather/current-weather.html',
-                  {'current_weather_data': response.json(),
+                  {'current_weather_data': response_current.json(),
+                   'forecast_current_weather': response_forecast.json(),
                    'query': query})
