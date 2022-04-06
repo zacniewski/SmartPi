60e9741328ef4781ee08a0b0da01612d7291325a
Artur Zacniewski <a.zacniewski@gmail.com>
2022-04-06 20:41:13 +0200

    Model Song for storing data about songs

--- apps/youtube/models.py
+++ apps/youtube/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Song(models.Model):
+    url = models.URLField()
+    title = models.CharField(max_length=100)
+    downloaded = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.title}"
