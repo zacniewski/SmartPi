5b8ed6d01e1e559e784c79ec675fb1353c8f5438
Artur Zacniewski <a.zacniewski@gmail.com>
2022-04-09 20:49:16 +0200

    View for playing and downloading mp3 files in the browser

--- apps/youtube/urls.py
+++ apps/youtube/urls.py
@@ -6,4 +6,5 @@ app_name = 'youtube'
 urlpatterns = [
     path('extracted/', views.extracted, name='extracted'),
     path('downloading/', views.downloader, name='downloader'),
+    path('download/<str:path>/', views.download_file, name='download_file'),
 ]
--- apps/youtube/views.py
+++ apps/youtube/views.py
@@ -1,4 +1,10 @@
+import glob
+import mimetypes
+import os
+
+from django.conf import settings as project_settings
 from django.contrib import messages
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
 
 from .forms import SongForm
@@ -7,26 +13,45 @@ from .tasks import yt_downloader
 
 
 def extracted(request):
+    song_list = []
+    dir_path = project_settings.MEDIA_ROOT
+    for full_path in glob.iglob(dir_path + '/' + '*.mp3'):
+        print(f"{full_path=}")
+        song_list.append([full_path, os.path.basename(full_path)])
+
     if request.method == "POST":
         form = SongForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
             url = cd['url']
-            # title = cd['title']
             if Song.objects.filter(url=url).exists():
                 messages.warning(request, 'This file was already downloaded!')
                 return redirect('youtube:extracted')
             else:
-                # new_song = form.save(commit=False)
-                # new_song.title = title
                 form.save()
             yt_downloader.delay(url)  # launch asynchronous task
             return redirect('youtube:downloader')
     else:
         form = SongForm()
     return render(request, 'youtube/extracted-songs.html',
-                  {'form': form})
+                  {'form': form,
+                   'song_list': song_list})
 
 
 def downloader(request):
     return render(request, 'youtube/downloading-song.html')
+
+
+def download_file(request, path):
+    file_path = os.path.join(project_settings.MEDIA_ROOT, path)
+    if os.path.exists(file_path):
+        with open(file_path, "rb") as fh:
+            mime_type, _ = mimetypes.guess_type(file_path)
+            response = HttpResponse(fh.read(), content_type=mime_type)
+            response["Content-Disposition"] = "inline; filename=" + os.path.basename(
+                file_path
+            )
+            return response
+        raise Http404("Nie ma takiego pliku!")
+    else:
+        return render(request, "youtube/404.html")
