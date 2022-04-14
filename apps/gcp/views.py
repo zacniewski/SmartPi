f9ac4641fb933d1289f68b7bd2b054be80c8f0fb
Artur Zacniewski <a.zacniewski@gmail.com>
2022-04-14 12:39:45 +0200

    Check new text of TTS

--- apps/gcp/views.py
+++ apps/gcp/views.py
@@ -8,7 +8,7 @@ def text_to_speech(request):
     client = texttospeech.TextToSpeechClient()
 
     # Set the text input to be synthesized
-    synthesis_input = texttospeech.SynthesisInput(text="Hello, World!")
+    synthesis_input = texttospeech.SynthesisInput(text="Thanks, later bitches!")
 
     # Build the voice request, select the language code ("en-US") and the ssml
     # voice gender ("neutral")
